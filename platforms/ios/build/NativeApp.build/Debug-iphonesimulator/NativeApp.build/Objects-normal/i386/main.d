ab75d410928c737e3152389fdaa26b6bbf47e8e9
Enrique Valencia <enriquev@ordersinseconds.com>
2017-09-15 11:30:10 -0600

    nativescript app

---
+++ platforms/ios/build/NativeApp.build/Debug-iphonesimulator/NativeApp.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/NativeApp/NativeApp-Prefix.pch \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/main.m \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/TNSDebugging.h \
+  /Users/oisdev2/Desktop/native/NativeApp/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
